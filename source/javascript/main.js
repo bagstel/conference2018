46da95a20137a51efc01994735cf3c6f3e664f10
Stelios Baglaridis <stelios@baglaridis.com>
2018-11-09 00:50:48 +0200

    hamburger fixed. page scrolling disabled

--- source/javascript/main.js
+++ source/javascript/main.js
@@ -1,7 +1,9 @@
 // hamburger
 const burger = document.querySelector('#burger');
 const nav = document.querySelector('.navigation');
+const page = document.querySelector('#fullpage');
 burger.addEventListener('click', () => {
   burger.classList.toggle("active");
   nav.classList.toggle("open");
+  page.classList.toggle("no-scroll");
 });
