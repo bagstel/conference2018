47cef0f7a65998e07e18db30805c1c5b12404a76
Stelios Baglaridis <stelios@baglaridis.com>
2018-11-10 22:32:49 +0200

    bags fixed.

--- source/javascript/main.js
+++ source/javascript/main.js
@@ -1,6 +1,6 @@
 // polyfill for "closest"
 if (!Element.prototype.closest) {
-    Element.prototype.closest = function(s) {
+    Element.prototype.closest = function (s) {
         let el = this;
         if (!document.documentElement.contains(el)) return null;
         do {
@@ -34,25 +34,28 @@ let widthWindow = document.body.clientWidth;
 window.addEventListener('resize', () => {
     widthWindow = document.body.clientWidth;
 });
+
 let handle;
-function interval () {
-  clearInterval(interval);
-  handle = setInterval(function () {
-    moveSlider('next');
-  }, 2000);
+
+function interval() {
+    clearInterval(interval);
+    handle = setInterval(function () {
+        moveSlider('next');
+    }, 2000);
 }
-function stop () {
-  clearInterval(handle);
-};
+
+function stop() {
+    clearInterval(handle);
+}
 interval();
 
-function moveSlider (e) {
+function moveSlider(e) {
     stop();
     let type = e;
     let itemWidth = sliderList.firstElementChild.offsetWidth + parseInt(getComputedStyle(sliderList.firstElementChild).marginRight) * 2
 
-    type === 'next' ? indexActive += 1 : indexActive -= 1
-    if  (widthWindow > 1284) {
+    type === 'next' ? indexActive += 1 : indexActive -= 1;
+    if (widthWindow > 1284) {
         step = 3;
     }
     else if (widthWindow > 751) {
@@ -65,33 +68,22 @@ function moveSlider (e) {
     if (indexActive > sliderList.children.length - step) indexActive = 0;
     sliderList.style.left = indexActive * (-itemWidth) + 'px';
     interval();
-};
+}
+
 cPrev.addEventListener('click', () => moveSlider('prev'));
 cNext.addEventListener('click', () => moveSlider('next'));
-// const moveSlider = function (e) {
-//   clearInterval(interval);
-//   let type;
-//   (e.target.closest('.speakers__contrl--prev')) ? type = 'prev' : type = 'next';
-//
-//   if (type === 'next') {
-//     sliderList.appendChild(sliderList.firstElementChild)
-//   }
-//   if (type === 'prev') {
-//     sliderList.insertBefore(sliderList.lastElementChild, sliderList.firstElementChild)
-//   }
-//   //interval();
-// };
 
 // scroll
 let links = document.querySelectorAll('[href^="#"]');
 let speed = 0.2;  // скорость, может иметь дробное значение через точку (чем меньше значение - тем больше скорость)
+let header = document.querySelector(".header__content");
+let heightHeader = header.offsetHeight;
 
 for (let n = 0; n < links.length; n++) {
 
     links[n].addEventListener('click', function (e) {
 
         e.preventDefault();
-
         burger.classList.remove("active");
         nav.classList.remove("open");
         page.classList.remove("no-scroll");
@@ -100,6 +92,8 @@ for (let n = 0; n < links.length; n++) {
         let hash = this.href.replace(/[^#]*(.*)/, '$1');
         let t = document.querySelector(hash).getBoundingClientRect().top;
         let start = null;
+
+        document.querySelector(hash).style.paddingTop = 0 + "px";
 
         requestAnimationFrame(step);
 
@@ -110,10 +104,8 @@ for (let n = 0; n < links.length; n++) {
 
             let progress = time - start;
             let r = (t < 0 ? Math.max(w - progress / speed, w + t) : Math.min(w + progress / speed, w + t));
-            let header = document.querySelector(".header__content");
-            let heightHeader = header.offsetHeight;
 
-            window.scrollTo(0, r - heightHeader); //корректировка на высоту header, который имеет position:fixed;
+            window.scrollTo(0, r);
 
             if (r !== w + t) {
                 requestAnimationFrame(step);
@@ -121,34 +113,21 @@ for (let n = 0; n < links.length; n++) {
             else {
                 location.hash = hash;
             }
-
         }
+
+        let sections = document.querySelectorAll("section");
+        console.log(111, sections);
+        sections.forEach(item => {
+            item.style.paddingTop = 0 + "px";
+    });
+
+        document.querySelector(hash).style.paddingTop = parseInt(document.querySelector(hash).style.paddingTop) + heightHeader + "px";
+        sections[0].style.paddingTop = 0 + "px";
+        sections[sections.length - 1].style.paddingTop = 0 + "px";
     });
 }
 
-
 //program
-// const program = document.querySelector('.program__list');
-// const items = program.querySelectorAll('.program__item');
-//
-// let toggle = function (e) {
-//     let target = e.target;
-//     let trigger = target.closest('.program__trigger');
-//     let active = target.closest('.program__item');
-//     if (!trigger) return;
-//     // if (active.classList.contains('program__item--active')) {
-//     //     active.classList.remove('program__item--active');
-//     // } else {
-//         items.forEach(item => {
-//             item.classList.remove('program__item--active');
-//         });
-//         active.classList.add('program__item--active');
-//     // }
-// };
-//
-// program.addEventListener('click', toggle);
-
-
 const program = document.querySelectorAll('.trigger');
 
 let toggle = function (e) {
@@ -177,51 +156,71 @@ for (let i = 0; i < program.length; i++) {
 
 // таймер обратного отсчета
 function getTimeRemaining(endtime) {
-  let t = Date.parse(endtime) - Date.parse(new Date());
-  let seconds = Math.floor((t / 1000) % 60);
-  let minutes = Math.floor((t / 1000 / 60) % 60);
-  let hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-  let days = Math.floor(t / (1000 * 60 * 60 * 24));
-  return {
-    total: t,
-    days: days,
-    hours: hours,
-    minutes: minutes,
-    seconds: seconds
-  };
+    let t = Date.parse(endtime) - Date.parse(new Date());
+    let seconds = Math.floor((t / 1000) % 60);
+    let minutes = Math.floor((t / 1000 / 60) % 60);
+    let hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+    let days = Math.floor(t / (1000 * 60 * 60 * 24));
+    return {
+        total: t,
+        days: days,
+        hours: hours,
+        minutes: minutes,
+        seconds: seconds
+    };
 }
 
 function initializeClock(id, endtime) {
-  let clock = document.getElementById(id);
-  let daysSpan = clock.querySelector(".days");
-  let hoursSpan = clock.querySelector(".hours");
-  let minutesSpan = clock.querySelector(".minutes");
-  let secondsSpan = clock.querySelector(".seconds");
+    let clock = document.getElementById(id);
+    let daysSpan = clock.querySelector(".days");
+    let hoursSpan = clock.querySelector(".hours");
+    let minutesSpan = clock.querySelector(".minutes");
+    let secondsSpan = clock.querySelector(".seconds");
 
-  function updateClock() {
-    let t = getTimeRemaining(endtime);
+    function updateClock() {
+        let t = getTimeRemaining(endtime);
 
-    if (t.total <= 0) {
-      document.getElementById("clockdiv").className = "hidden";
-      document.getElementById("clockdiv2").className = "hidden";
-      document.getElementById("deadline-message").className = "visible";
-      document.getElementById("deadline-message2").className = "visible";
-      clearInterval(timeinterval);
-      return true;
+        if (t.total <= 0) {
+            document.getElementById("clockdiv").className = "hidden";
+            document.getElementById("clockdiv2").className = "hidden";
+            document.getElementById("deadline-message").className = "visible";
+            document.getElementById("deadline-message2").className = "visible";
+            clearInterval(timeinterval);
+            return true;
+        }
+
+        daysSpan.innerHTML = t.days;
+        hoursSpan.innerHTML = ("0" + t.hours).slice(-2);
+        minutesSpan.innerHTML = ("0" + t.minutes).slice(-2);
+        secondsSpan.innerHTML = ("0" + t.seconds).slice(-2);
     }
 
-    daysSpan.innerHTML = t.days;
-    hoursSpan.innerHTML = ("0" + t.hours).slice(-2);
-    minutesSpan.innerHTML = ("0" + t.minutes).slice(-2);
-    secondsSpan.innerHTML = ("0" + t.seconds).slice(-2);
-  }
+    updateClock();
 
-  updateClock();
-  let timeinterval = setInterval(updateClock, 1000);
+    let timeinterval = setInterval(updateClock, 1000);
 }
 
-//var deadline = "January 01 2018 00:00:00 GMT+0300"; //for Ukraine
 let deadline = "December 03 2018 00:00:00 GMT+0300";
-//var deadline = new Date(Date.parse(new Date()) + 5 * 1000); // for endless timer
+
 initializeClock("clockdiv", deadline);
 initializeClock("clockdiv2", deadline);
+
+let moveLayers = (e) => {
+    const parallaxContainer = document.querySelector("#parallax");
+    const layer = parallaxContainer.querySelector(".parallax__layer");
+
+    let initialX = (window.innerWidth / 2) - e.pageX;
+    let initialY = (window.innerHeight / 2) - e.pageY;
+
+    let divider = 10 / 100;
+    let positionX = initialX * divider;
+    let positionY = initialY * divider;
+    let bottomPosition = (window.innerHeight / 2) * divider;
+    let layerStyle = layer.style;
+    let transformString = `translate3d(${positionX}px, ${positionY}px, 0)`;
+
+    layerStyle.transform = transformString;
+    layerStyle.bottom = `-${bottomPosition}px`;
+};
+
+window.addEventListener('mousemove', moveLayers);
