059bc75641f02063b9613d70623de147515fb96d
Stelios Baglaridis <stelios@baglaridis.com>
2018-11-11 12:08:17 +0200

    section-6 bag fixed

--- source/javascript/libs/scroll-and-burger.js
+++ source/javascript/libs/scroll-and-burger.js
@@ -13,46 +13,52 @@ module.exports = (function () {
 // scroll
     let links = document.querySelectorAll('[href^="#"]');
     let speed = 0.2;  // скорость, может иметь дробное значение через точку (чем меньше значение - тем больше скорость)
+    let header = document.querySelector(".header__content");
+    let heightHeader = header.offsetHeight;
 
     for (let n = 0; n < links.length; n++) {
 
-        links[n].addEventListener('click', function (e) {
+      links[n].addEventListener('click', function (e) {
 
-            e.preventDefault();
+        e.preventDefault();
+        burger.classList.remove("active");
+        nav.classList.remove("open");
+        page.classList.remove("no-scroll");
 
-            burger.classList.remove("active");
-            nav.classList.remove("open");
-            page.classList.remove("no-scroll");
+        let w = window.pageYOffset;
+        let hash = this.href.replace(/[^#]*(.*)/, '$1');
+        let t = document.querySelector(hash).getBoundingClientRect().top;
+        let start = null;
 
-            let w = window.pageYOffset;
+        document.querySelector(hash).style.paddingTop = 0 + "px";
+        requestAnimationFrame(step);
 
-            let hash = this.href.replace(/[^#]*(.*)/, '$1');
-            let t = document.querySelector(hash).getBoundingClientRect().top;
+        function step (time) {
+          if (start === null) {
+            start = time;
+          }
 
-            let start = null;
+          let progress = time - start;
+          let r = (t < 0 ? Math.max(w - progress / speed, w + t) : Math.min(w + progress / speed, w + t));
 
+          window.scrollTo(0, r);
+
+          if (r !== w + t) {
             requestAnimationFrame(step);
+          }
+          else {
 
-            function step(time) {
-                if (start === null) {
-                    start = time;
-                }
+            location.hash = hash;
+          }
+        }
 
-                let progress = time - start;
-
-                let r = (t < 0 ? Math.max(w - progress / speed, w + t) : Math.min(w + progress / speed, w + t));
-
-                window.scrollTo(0, r);
-
-                if (r !== w + t) {
-                    requestAnimationFrame(step);
-                }
-                else {
-
-                    location.hash = hash;
-                }
-            }
+        let sections = document.querySelectorAll("section");
+        sections.forEach(item => {
+          item.style.paddingTop = 0 + "px";
         });
+        document.querySelector(hash).style.paddingTop = parseInt(document.querySelector(hash).style.paddingTop) + heightHeader + "px";
+        sections[0].style.paddingTop = 0 + "px";
+        sections[sections.length - 1].style.paddingTop = 0 + "px";
+      })
     }
-
 })();
